a2d6b7090ff99125a58ea1a3390644d566b71b0c
Martin Wilck <mwilck@suse.com>
2023-01-06 15:34:27 +0100

    install.sh: install NvmeOfCli, and automate HEAD detection

--- install.sh
+++ install.sh
@@ -7,9 +7,14 @@ FLAVOR=$1
 : "${GDB:=/mnt/git/tmp/nvme/gdb}"
 : "${OVMF:=/mnt/git/tmp/nvme}"
 : "${OVMF2:=/mnt/nv/mwilck/nvme-poc/ovmf}"
+: "${EFITOOLS:=/mnt/nv/mwilck/nvme-poc/efitools}"
 
 SD=Build/OvmfX64/DEBUG_GCC5
-COMMIT=$(git describe --tags HEAD~)
+COMMIT=HEAD
+while git show $COMMIT | grep -q a/install.sh; do
+    COMMIT="$COMMIT"~
+done
+COMMIT=$(git describe --tags $COMMIT)
 NAME=$COMMIT-$FLAVOR
 
 [ ! -d "$GDB/$NAME" ]
@@ -21,3 +26,4 @@ cp -v "$SD/FV/OVMF_CODE.fd" "$OVMF/$NAME-code.bin"
 cp -v "$SD/FV/OVMF_VARS.fd" "$OVMF/$NAME-vars.bin"
 ln -v "$SD/FV/OVMF_CODE.fd" "$OVMF2/$NAME-code.bin"
 ln -v "$SD/FV/OVMF_VARS.fd" "$OVMF2/$NAME-vars.bin"
+cp -v "$SD/X64/NvmeOfCli.efi" "$EFITOOLS/NvmeOfCli-$NAME.efi"
